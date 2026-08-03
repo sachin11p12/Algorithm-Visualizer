bdbdcd9eeed8aab1a71c5e8538627206fea9c599
Sachin11p12 <sachin11p12@gmail.com>
2026-08-03 13:06:52 +0530

    feat: initialize root layout with theme provider, fonts, and metadata configuration

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/layout/ThemeProvider';
+import { log } from 'console';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -32,5 +33,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+
   );
 }
